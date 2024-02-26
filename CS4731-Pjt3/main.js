e2772d2535df57d8f416cb7d716aff776f8a3020
JohnnyWobble <tonald.drump2.0@gmail.com>
2024-02-26 12:50:11 -0500

    Proj3 start

---
+++ CS4731-Pjt3/main.js
@@ -0,0 +1,70 @@
+let canvas;
+let gl;
+let program;
+
+let stopSign;
+let lamp;
+let car;
+let street;
+let bunny;
+
+function startPipeline() {
+
+}
+
+function main() {
+    // Retrieve <canvas> element
+    canvas = document.getElementById('webgl');
+
+    // Get the rendering context for WebGL
+    gl = WebGLUtils.setupWebGL(canvas, undefined);
+
+    //Check that the return value is not null.
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    // Set viewport
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    // Set clear color
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Initialize shaders
+    program = initShaders(gl, "vshader", "fshader");
+    gl.useProgram(program);
+
+    // Get the stop sign
+    stopSign = new Model(
+        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.obj",
+        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/stopsign.mtl");
+
+    // Get the lamp
+    lamp = new Model(
+        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/lamp.obj",
+        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/lamp.mtl");
+
+    // Get the car
+    car = new Model(
+        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/car.obj",
+        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/car.mtl");
+
+   // Get the street
+    street = new Model(
+        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/street.obj",
+        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/street.mtl");
+
+    // Get the bunny (you will not need this one until Part II)
+    bunny = new Model(
+        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/bunny.obj",
+        "https://web.cs.wpi.edu/~jmcuneo/cs4731/project3/bunny.mtl");
+
+    // Wait for obj and texture loading to finish before running the rest of the program
+    let intCallbk = setInterval(() => {
+        if (stopSign.ready && lamp.ready && car.ready && street.ready && bunny.ready) {
+            clearInterval(intCallbk);
+            startPipeline();
+        }
+    }, 20);
+}
