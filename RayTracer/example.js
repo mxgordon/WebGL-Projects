03da7effa0de39f77cf4aab71eb7f301a42899e5
Max Gordon <mgordon291us@gmail.com>
2024-09-07 12:15:52 -0500

    Added ray tracer & index page

---
+++ RayTracer/example.js
@@ -0,0 +1,57 @@
+let program;
+let gl;
+
+// JS callback to change the scene in the canvas
+function changeScene(scene) {
+    gl.uniform1i(gl.getUniformLocation(program, 'sceneNumber'), scene);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+function main()
+{
+    // Retrieve <canvas> element
+    var canvas = document.getElementById('webgl');
+
+    // Get the rendering context for WebGL
+    gl = WebGLUtils.setupWebGL(canvas, undefined);
+    if (!gl)
+    {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    program = initShaders(gl, "vshader", "fshader");
+    gl.useProgram(program);
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+
+    // Set clear color
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
+    // set the scene number uniform variable
+    gl.uniform1i(gl.getUniformLocation(program, 'sceneNumber'), 1);
+
+    var buffer = gl.createBuffer();
+
+    // Create a square as a strip of two triangles.
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            -1,1,
+            0,1,
+            1,0,
+            -1,-1,
+            0,1,
+            -1,0]),
+        gl.STATIC_DRAW
+    );
+
+    gl.aPosition = gl.getAttribLocation(program, "aPosition");
+    gl.enableVertexAttribArray(gl.aPosition);
+    gl.vertexAttribPointer(gl.aPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
